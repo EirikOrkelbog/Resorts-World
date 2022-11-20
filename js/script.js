cb4f75cc4b43194dfa0902b7e8348f21a71e18a1
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2022-11-20 14:16:56 +0100

    Added hover effect on links

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // Nav arrow 
 
+const navCircleContainer = document.querySelector('.nav__circle-container');
 const navCircle = document.querySelector('.nav__circle');
 const navArrow = document.querySelector('.nav__hotels-arrow');
 const navArrowHidden = document.querySelector('.arrow-hidden');
 
 
-navCircle.addEventListener('mouseenter', handleNavCircleEnter);
-navCircle.addEventListener('mouseleave', handleNavCircleLeave);
+navCircleContainer.addEventListener('mouseenter', handleNavCircleEnter);
+navCircleContainer.addEventListener('mouseleave', handleNavCircleLeave);
 
 function handleNavCircleEnter(event) {
 	translateArrowEnter();
@@ -26,3 +27,13 @@ function translateArrowLeave() {
 	navArrow.style.transform = 'translate(-50%, -50%)';
 	navArrowHidden.style.transform = 'translate(-400%, -50%)';
 }
+
+
+// Changing color 
+const [red, green, blue] = [216, 41, 58]
+
+window.addEventListener('scroll', () => {
+  const y = 1 + (window.scrollX || window.pageXOffset) / 150
+  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+  navCircle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+})
