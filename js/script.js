86960441492a169da6973e76791d4e3a36f40970
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2022-11-20 15:24:21 +0100

    Added dark hover effect on hotel links

--- js/script.js
+++ js/script.js
@@ -37,3 +37,33 @@ window.addEventListener('scroll', () => {
   const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
   navCircle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 })
+
+
+
+// Hover on hotel links in nav 
+
+const hotelLink = document.querySelectorAll('.hotel__link');
+const backgroundDark = document.querySelector('.background__dark');
+
+for (i = 0; i < hotelLink.length; i += 1) {
+	hotelLink[i].addEventListener('mouseenter', handleHotelLinkMouseenter);
+	hotelLink[i].addEventListener('mouseleave', handleHotelLinkMouseleave);
+}
+
+function handleHotelLinkMouseenter(event) {
+	showImageandDarkBackground();
+}
+
+function handleHotelLinkMouseleave(event) {
+	hideImageandDarkBackground();
+}
+
+function showImageandDarkBackground() {
+	backgroundDark.classList.add('background__dark-hover');
+	backgroundDark.classList.remove('background__light-hover');
+}
+
+function hideImageandDarkBackground() {
+	backgroundDark.classList.remove('background__dark-hover');
+	backgroundDark.classList.add('background__light-hover');
+}
