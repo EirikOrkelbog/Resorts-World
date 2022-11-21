1c20088fa02fa7e5d238a8dab1cb931a79c3af4e
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2022-11-21 23:23:04 +0100

    Added moving text on about circle

--- js/script.js
+++ js/script.js
@@ -30,13 +30,13 @@ function translateArrowLeave() {
 
 
 // Changing color on start planning circle 
-const [red, green, blue] = [216, 41, 58]
+// const [red, green, blue] = [216, 41, 58]
 
-window.addEventListener('scroll', () => {
-  const y = 1 + (window.scrollX || window.pageXOffset) / 150
-  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
-  navCircle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-})
+// window.addEventListener('scroll', () => {
+//   const y = 1 + (window.scrollX || window.pageXOffset) / 150
+//   const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+//   navCircle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+// })
 
 
 
@@ -170,6 +170,25 @@ function videoPlayPause() {
 }
 
 
+// about pause video
+const aboutVideo = document.querySelector('.about__pause-video');
+const aboutBackgroundVideo = document.querySelector('.about__video');
+
+aboutVideo.addEventListener('click', handleAboutPauseClick);
+
+function handleAboutPauseClick(event) {
+	aboutVideoPlayPause();
+}
+
+function aboutVideoPlayPause() {
+	if (aboutBackgroundVideo.paused) {
+		aboutBackgroundVideo.play();
+	} else {
+		aboutBackgroundVideo.pause();
+	}
+}
+
+
 // Horizontal scroll
 // const main = document.querySelector('.main');
 
@@ -179,3 +198,57 @@ function videoPlayPause() {
 // 	left: event.deltaY < 0 ? -30 : 30
 // 	}); 
 // });
+
+
+
+
+// Moving text on about circle
+const aboutCircle = document.querySelector('.about__play-circle');
+const circleText = document.querySelectorAll('.circle__play-sound');
+
+aboutCircle.addEventListener('mouseenter', handleCircleEnter);
+aboutCircle.addEventListener('mouseleave', handleCircleLeave);
+
+
+function handleCircleEnter(event) {
+	expandCircleText();
+}
+
+function handleCircleLeave(event) {
+	resetCircleText();
+}
+
+function expandCircleText() {
+	circleText[0].style.marginTop = '1.3rem';
+	circleText[1].style.marginBottom = '1.3rem';
+}
+
+function resetCircleText() {
+	circleText[0].style.marginTop = '.8rem';
+	circleText[1].style.marginBottom = '.8rem';
+}
+
+
+
+// expand hotels
+
+const content = document.querySelector('.expand__content-container');
+const expandContainer = document.querySelector('.entertainment__expand-container');
+
+expandContainer.addEventListener('click', handleExpandOnClick);
+
+function handleExpandOnClick(event) {
+	closeAndOpenExpandContainer();
+}
+	
+function closeAndOpenExpandContainer() {
+	content.style.display = 'none';	
+	expandContainer.style.width = '33.3rem';
+}
+
+
+
+// for (i = 0; i < expandContainer.length; i += 1) {
+// 	}
+// const hotelsContent = content.index;
+// console.log(hotelsContent);
